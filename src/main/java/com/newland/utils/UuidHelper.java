b6e10e9b183f14d709ebe551de9dec2fa2f74014
laidingqing <laidingqing@gmail.com>
2011-04-22 11:49:10 +0800

    正贫困户

--- src/main/java/com/newland/utils/UuidHelper.java
+++ src/main/java/com/newland/utils/UuidHelper.java
@@ -1,15 +1,14 @@
 package com.newland.utils;
 
 
-import java.util.UUID;
 
-import com.fasterxml.uuid.Generators;
+import java.util.UUID;
 
 public class UuidHelper {
 
 	public static String generateUUID(){
-		UUID uid = Generators.timeBasedGenerator().generate();
-		return uid.toString();
+		//UUID uid = Generators.timeBasedGenerator().generate();
+		return UUID.randomUUID().toString();
 	}
 }
 
