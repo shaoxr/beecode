ab3391d1d9cc88ea1f8d5327e9e6c27ea75e7f87
laidingqing <laidingqing@gmail.com>
2011-04-26 11:06:53 +0800

    remove target

--- src/main/java/com/newland/beecode/web/DictViewController.java
+++
@@ -1,7 +0,0 @@
-package com.newland.beecode.web;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public class DictViewController {
-}
