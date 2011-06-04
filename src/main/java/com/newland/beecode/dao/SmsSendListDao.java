a8be4dbfc51831fdfabb831ed60afd6388b94694
shaoxr <shaoxr1919@gmail.com>
2011-06-04 12:02:11 +0800

    a

---
+++ src/main/java/com/newland/beecode/dao/SmsSendListDao.java
@@ -0,0 +1,10 @@
+package com.newland.beecode.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.newland.beecode.domain.SmsSendList;
+
+@Repository("smsSendListDao")
+public class SmsSendListDao extends BaseDao<SmsSendList,Long>{
+
+}
