ded8be2d3efe4780f98e1e2a5204fe010bec7709
shaoxr <shaoxr1919@gmail.com>
2011-06-04 11:49:41 +0800

    a

---
+++ src/main/java/com/newland/utils/BarCodeFormat.java
@@ -0,0 +1,27 @@
+package com.newland.utils;
+
+import com.newland.beecode.domain.Coupon;
+import com.newland.beecode.domain.MarketingAct;
+
+/**
+ * @author shaoxr:
+ * @version 2011-5-25 下午10:04:27
+ * 
+ */
+public class BarCodeFormat {
+	
+	public static String genBarCodeCode(Coupon coupon,MarketingAct act){
+		StringBuffer sb=new StringBuffer();
+		sb.append(coupon.getBusinessType());
+		if(act.getBizNo().equals(Coupon.BIZ_TYPE_VOUCHER)){
+			sb.append(NewlandUtil.formatBigDecimal(coupon.getBackAmount(), 10));
+		}else{
+			sb.append(NewlandUtil.formatBigDecimal(act.getRebateRate(), 10));
+		}
+		sb.append(NewlandUtil.dataToString(act.getEndDate(), "yyyyMMddHHmm"));
+		sb.append(coupon.getSerialNo());
+		return sb.toString();
+		
+	}
+
+}
