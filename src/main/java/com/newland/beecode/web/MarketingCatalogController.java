31c29cb9c4fac3eac3dd0bbbfc16b8233511c165
tangyao@gmail.com <tangyao16@gmail.com>
2011-06-21 15:35:26 +0800

    多页删除无法跳到前一页

--- src/main/java/com/newland/beecode/web/MarketingCatalogController.java
+++ src/main/java/com/newland/beecode/web/MarketingCatalogController.java
@@ -1,6 +1,7 @@
 package com.newland.beecode.web;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -68,6 +69,10 @@ public class MarketingCatalogController extends BaseController{
     		@RequestParam(value = "size", required = false) Integer size, Model model) {
             try {
 				this.marketingCatalogService.delete(id);
+				List<MarketingCatalog> marketingCatalog = this.marketingCatalogService.findMarketingCatalogEntries((page.intValue() - 1) * size, size);
+    			if(page.intValue()>1 && marketingCatalog.size()==0){
+    				page-=1;
+    			}
 				 model.addAttribute("page", (page == null) ? "1" : page.toString());
 			     model.addAttribute("size", (size == null) ? PaginationHelper.PAGE_SIZE : size.toString());
 			} catch (Exception e) {
